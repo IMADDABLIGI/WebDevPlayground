1b50c2cff62198705c5def2b451b5e53723c8892
Imad Dabligi <imad.dabligi@gmail.com>
2024-03-10 11:19:42 +0000

    refactor (ErrorPage) Create Components

---
+++ ErrorPage/src/components/ErrorPage/Button.jsx
@@ -0,0 +1,10 @@
+function Button() {
+    return (
+        <div className="button">
+            <strong> <a href="https://github.com/elmehdi-kenane/ft_transcendence"
+                target="_blank" > BACK TO HOME </a></strong>
+          </div>
+    );
+}
+
+export default Button;
