b4ad0db70101046b0b74012f11917b4a7d7b180c
Imad Dabligi <imad.dabligi@gmail.com>
2024-04-16 14:16:43 +0100

    (ReactWebsite) - Reordering

--- reactwebsite/src/component/home/Home.jsx
+++ reactwebsite/src/component/home/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import NavBar from '../navbar/NavBar'
-import Main from './Main'
 import Footer from '../footer/Footer'
+import Image from '../../assets/pizza.jpeg'
+import {Link} from 'react-router-dom'
+import '../../styles/Home.css'
+
 
 function Home() {
   return (
     <>
       <NavBar />
-      <Main />
+      <div className='main' style={{backgroundImage:`url(${Image})`}}>
+        <h1> Pizzeria </h1>
+        <p> PIZZA TO FIT ANYTASTE </p>
+        <Link to='/menu' className='orderButton'> Order Now </Link>
+      </div>
       <Footer />
     </>
   )
