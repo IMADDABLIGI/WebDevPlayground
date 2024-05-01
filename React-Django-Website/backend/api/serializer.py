e2a9cb5fe5dc10816ac7f5ec1f0f35076a091365
Imad Dabligi <imad.dabligi@gmail.com>
2024-05-01 15:24:31 +0100

    (Backend)/ Authentication Users Done

--- React-Django-Website/backend/api/serializer.py
+++ React-Django-Website/backend/api/serializer.py
@@ -1,12 +1,43 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-class UserSerialaizer(serializers.ModelSerializer):
+def checkPassword(password):
+    if len(password) < 6:
+        raise serializers.ValidationError("Password is too short")
+    is_upper = False
+    is_digit = False
+    for e in password:
+        if e.isupper():
+            is_upper = True
+        if e.isdigit():
+            is_digit = True
+    if is_digit == False or is_upper == False:
+        raise serializers.ValidationError("Password should have at least one Capital Letter and a Digit")
+
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "email", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        email = validated_data["email"]
+        password = validated_data["password"]
+        username = validated_data.get("email")
+
+        user = User.objects.create_user(email=email, password=password, username=username)
         return user
+    
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+        
+        if email and User.objects.filter(email=email).exists(): # Check if email already exists
+            raise serializers.ValidationError("Email address already exists.")
+
+        # checkPassword(password)
+
+        return attrs
+    
+
--- React-Django-Website/backend/api/views.py
+++ React-Django-Website/backend/api/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from .serializer import UserSerializer
 
 # Create your views here.
 
 class CreateUser(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerialaizer
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
