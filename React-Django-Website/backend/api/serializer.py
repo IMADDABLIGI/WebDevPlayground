c7ceae7622eefc4e464838a6afaba1ac299216dd
Imad Dabligi <imad.dabligi@gmail.com>
2024-05-03 19:47:43 +0100

    (Backend)/ Authentication Users Done

--- React-Django-Website/backend/api/serializer.py
+++ React-Django-Website/backend/api/serializer.py
@@ -18,26 +18,11 @@ def checkPassword(password):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "email", "password"]
+        fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        email = validated_data["email"]
+        username = validated_data["username"]
         password = validated_data["password"]
-        username = validated_data.get("email")
-
-        user = User.objects.create_user(email=email, password=password, username=username)
-        return user
-    
-    def validate(self, attrs):
-        email = attrs.get("email")
-        password = attrs.get("password")
-        
-        if email and User.objects.filter(email=email).exists(): # Check if email already exists
-            raise serializers.ValidationError("Email address already exists.")
-
-        # checkPassword(password)
-
-        return attrs
-    
-
+        user = User.objects.create_user(username=username, password=password)
+        return (user)
