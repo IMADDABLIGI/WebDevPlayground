f795df95de70ae6c8d0d4eb7ee503b154a8ab776
Imad Dabligi <imad.dabligi@gmail.com>
2024-05-06 17:12:01 +0100

    (frontend)/ Handle Authentication Errors

--- React-Django-Website/frontend/src/component/register/Register.jsx
+++ React-Django-Website/frontend/src/component/register/Register.jsx
@@ -10,20 +10,37 @@ function Register() {
     const [username, setUserName] = useState("")
     const [password, setPassword] = useState("")
 
+    const isValidEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      };      
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(username, password)
+        console.log(username, password);
+        if (isValidEmail(username) === false)
+            alert ("Please enter a valid email address.")
+        else
+            try {
+                const res = await axios.post("http://127.0.0.1:8000/api/register/", {
+                    username, password })
+                if (res.status === 200){
+                    console.log("Register Success")
+                }
+            } 
+            catch (error) {
+                if (error.response) {
+                    console.log("Error response from server:", error.response.data);
+                    console.log("Status code:", error.response.status);
+                    
+                    if (error.response.data.username)
+                        alert(error.response.data.username[0])
 
-        try {
-            const res = await axios.post("http://127.0.0.1:8000/api/register/", {
-                username, password })
-            if (res.status === 200){
-                console.log("Register Success")
+                } else {
+                    // Something happened in setting up the request that triggered an error
+                    console.log("Error:", error.message);
+                }
             }
-        } 
-        catch (error) {
-            console.log(error)
-        }
     }
 
     return (
