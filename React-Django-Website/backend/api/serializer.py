da71abb84a6b1476a93210fe771eeec61e147d6e
Imad Dabligi <imad.dabligi@gmail.com>
2024-05-07 14:29:21 +0100

    (Backend)/ Authentication New Users

--- React-Django-Website/backend/api/serializer.py
+++ React-Django-Website/backend/api/serializer.py
@@ -1,20 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-def checkPassword(password):
-    if len(password) < 6:
-        raise serializers.ValidationError("Password is too short")
-    is_upper = False
-    is_digit = False
-    for e in password:
-        if e.isupper():
-            is_upper = True
-        if e.isdigit():
-            is_digit = True
-    if is_digit == False or is_upper == False:
-        raise serializers.ValidationError("Password should have at least one Capital Letter and a Digit")
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
